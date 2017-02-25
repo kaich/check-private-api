7ce190d40f886636f2c674b0002f6fdc4d374b1b
kaich <chengkai1853@163.com>
2017-02-25 11:44:55 +0800

    fix search bug

--- CheckIOSPrivateApi/app/models/api_result.rb
+++ CheckIOSPrivateApi/app/models/api_result.rb
@@ -2,9 +2,8 @@ class ApiResult < ActiveRecord::Base
   serialize :result, JSON
 
   def self.search(category,keyword,user_name)
-    debugger
     if keyword && category && user_name 
-      where(["user_name = '%s' and '%s' like '%s'", user_name, category,keyword])
+      where(["user_name = '%s' and %s like '%s'", user_name, category,keyword])
     else
       all 
     end
